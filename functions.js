3f240b24cef6dd653758f2690d5a9cca083b3619
Nowlook4D <contact@nowlook.fr>
2024-08-13 12:43:30 +0200

    camera fixe sur le booth

--- functions.js
+++ functions.js
@@ -1,4 +1,22 @@
 client.init(uid, { autostart: 1, autospin: 0, success: success, error: error });
+// Camera logs
+// const cameraData = {
+//   "useCameraConstraints": true,
+//   "usePanConstraints": false,
+//   "useZoomConstraints": true,
+//   "usePitchConstraints": true,
+//   "useYawConstraints": false,
+//   "zoomIn": 50,
+//   "zoomOut": 1500,
+  // rotations horizontales et verticales
+  // "left": -3.1416,
+  // "right": 3.1416,
+  // "up": 1.57,
+  // "down": 0.1,
+// };
+// var annotInitiale = [];
+
+
 //-----------------------------------------------------------
 function dialogue1(phrase) {
   popup1.textContent = "";
@@ -58,3 +76,84 @@ function changerModeleSketchfab(nouvelIdentifiant) {
   const nouvelleURL = `https://sketchfab.com/models/${nouvelIdentifiant}/embed`;
   iframe.src = nouvelleURL;
 }
+
+
+
+function computePastilles(wCanvas, hCanvas, bgColor, bgBorderColor, fgColor, fgBorderColor, text, numHotspot, wPastille, hPastille) {
+  var wSize = wPastille / 10.0;
+  var col = wCanvas / wSize;
+  var row = hCanvas / wSize;
+  var padding = 2;
+  var w = wSize - padding;
+  var cx;
+  var cy = w * 0.5;
+  var ty = cy + 8;
+  var pastille = '';
+  var num = 0;
+  for (var i = 0; i < row; i++) {
+    cx = wSize * 0.5;
+    for (var k = 0; k < col; k++) {
+      num++;
+      var letters = text === 0 ? num : text[num];
+      var circle = "<circle cx=\"".concat(cx, "\"\n            cy=\"").concat(cy, "\"\n            r=\"20\"\n            fill=\"").concat(bgColor, "\"\n            stroke=\"").concat(bgBorderColor, "\"\n            stroke-width=\"2\"/>");
+      var textVG = "<text font-size=\"26\"\n          stroke=\"".concat(fgBorderColor, "\"\n          fill=\"").concat(fgColor, "\"\n          font-family=\"sans-serif\"\n          text-anchor=\"middle\"\n          alignment-baseline=\"baseline\"\n          x=\"").concat(cx, "\"\n          y=\"").concat(ty, "\">").concat(letters, "</text>");
+      pastille += circle + textVG;
+      cx += wSize;
+    }
+    cy += wSize;
+    ty += wSize;
+  }
+  var s = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  s.setAttribute('version', '1.1');
+  s.setAttribute('baseProfile', 'full');
+  s.setAttribute('width', wPastille);
+  s.setAttribute('height', hPastille);
+  s.setAttribute('viewBox', "0 0 ".concat(wPastille, " ").concat(hPastille));
+  s.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  s.innerHTML = pastille;
+  // make it base64
+  var svg64 = btoa(s.outerHTML);
+  var b64Start = 'data:image/svg+xml;base64,';
+  var image64 = b64Start + svg64;
+  var textureOptions = {
+    url: image64,
+    colNumber: col,
+    padding: padding,
+    iconSize: w
+  };
+  return textureOptions;
+}
+
+function getNewPastilleURL(bgColor, bgBorderColor, fgColor, fgBorderColor, text, numHotspot, w, h) {
+  var imageData;
+  imageData = computePastilles(w, h, bgColor, bgBorderColor, fgColor, fgBorderColor, text, numHotspot, w, h);
+  return imageData;
+}
+
+client.init(uid, {
+annotation: 0, // Usage: Setting to [1 – 100] will automatically load that annotation when the viewer starts.
+annotations_visible: 1, // Usage: Setting to 0 will hide annotations by default.
+//   annotation_cycle: 0, // Déroule les annotations avec le nombre de secondes indiquées.
+autospin: 0, // Usage: Setting to any other number will cause the model to automatically spin around the z-axis after loading.
+autostart: 1, // Usage: Setting to 1 will make the model load immediately once the page is ready, rather than waiting for a user to click the Play button.
+camera: 1, // Usage: Setting to 0 will skip the initial animation that occurs when a model is loaded, and immediately show the model in its default position.
+ui_stop: 0, // Usage: Setting to 0 will hide the "Disable Viewer" button in the top right so that users cannot stop the 3D render once it is started.
+transparent: 1, // Usage: Setting to 1 will make the model's background transparent
+ui_animations: 1, // Usage: Setting to 0 will hide the animation menu and timeline.
+ui_annotations: 1, // Usage: Setting to 0 will hide the Annotation menu.
+ui_controls: 1, // Usage: Setting to 0 will hide all the viewer controls at the bottom of the viewer (Help, Settings, Inspector, VR, Fullscreen, Annotations, and Animations).
+ui_fullscreen: 0, // Usage: Setting to 0 will hide the Fullscreen button.
+ui_general_controls: 1, // Usage: Setting to 0 will hide main control buttons in the bottom right of the viewer (Help, Settings, Inspector, VR, Fullscreen).
+ui_help: 1, // Usage: Setting to 0 will hide the Help button.
+ui_hint: 0, // Usage: Setting to 0 will always hide the viewer hint animation ("click & hold to rotate"). Setting to 1 will show the hint the first time per browser session (using a cookie). Setting to 2 will always show the hint.
+ui_infos: 0, // Usage: Setting to 0 will hide the model info bar at the top of the viewer. Share sign etc.
+ui_inspector: 0, // Usage: Setting to 0 will hide the inspector button.
+ui_settings: 0, // Usage: Setting to 0 will hide the Settings button.
+ui_vr: 0, // Usage: Setting to 0 will hide the View in VR button.
+ui_watermark_link: 0, // Usage: Setting to 0 remove the link from the Sketchfab logo watermark.
+ui_color: '00a8c0', // Usage: Setting to a hexidecimal color code (without the #) or a HTML color name will change the color of the viewer loading bar.
+ui_watermark: 0, // Usage: Setting to 0 remove the Sketchfab logo watermark.
+
+success: success,
+error: error
+});
--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const version = '1.12.1';
 const iframe = document.getElementById('api-frame');
 var client = new window.Sketchfab(version, iframe);
 let api;
-// const uid = 'd52b36c6cc5343b387883f763ef2693d'; 
-const uid = '122c6f2e36c7475db0b065dd96441fcc';
+const uid = 'd7e6aa78623a4981b340dbdd15c71c70';
 
 var boutonAide = document.querySelector("input");
 
@@ -11,19 +10,7 @@ const error = () => window.console.error('Sketchfab API error');
 const success = apiClient => {
 api = apiClient; 
 
-const TObj = [
-    [527, 'Rectangular Backshell\nClick to shape your own design in 3D'],
-    [464, 'Rectangular Backshell\nClick to shape your own design in 3D'],
-    [55, 'Flexible flat cable Microflex Octopus'],
-    [77, 'Flexible flat cable Microflex Octopus'],
-    [102, 'Flexible flat cable Microflex Female-Female'],
-    [118, 'Flexible flat cable Microflex Female-Female'],
-    [166, 'Flexible flat cable Microflex Male-Female'],
-    [182, 'Flexible flat cable Microflex Male-Female'],
-    [244, 'CMM320 LF & Mixed Series 3 rows'],
-    [270, 'CMM340 Series 3 rows']
-];
-let text;
+
 
 //------------------------------------------------
 api.start(function () {
@@ -31,31 +18,12 @@ api.addEventListener('viewerready', () => {
     // api.getNodeMap(function(err, nodes) {
     //     if (!err) {window.console.log(nodes); }
     // });
-    // api.addEventListener('nodeMouseEnter', function (info) {
-    //         for (let i = 0; i < TObj.length; i++) {
-    //             if (TObj[i][0] == info.instanceID) {
-    //                 // text = TObj[i][1];
-    //                 var popup = document.getElementById('popup');
-    //                 var popupText = document.getElementById('popup-text');
-    //                 // console.log(info.position2D[0], info.position2D[1])
-    //                 // popupText.innerText = text;
-    //                 popupText.innerText = TObj[i][1];
-    //                 popup.style.left = info.position2D[0] + 70 + 'px';
-    //                 popup.style.top = info.position2D[1] + 300 + 'px';
-    //                 popup.style.display = 'block';
-    //             };
-    //         };
-    //         // console.log('nodeMouseEnter', info);
-    // }, {pick: 'fast' });
-
-
-    // api.addEventListener('nodeMouseLeave', function (info) {popup.style.display = 'none';}, {pick: 'fast'});
 
     api.addEventListener('click',function(info) { 
     if (info.instanceID) {  // le clic se fait effectivement sur un objet 
         window.console.log('clicked node', info.instanceID);
         // if ((info.instanceID == 464) || (info.instanceID == 527)) { 
-        if (info.instanceID == 338) { 
+        if ((info.instanceID == 73) || (info.instanceID == 91)) { 
             window.open('rect.html', '_blank');
         }
     }
