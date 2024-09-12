e84339f766837533d77a2eaff5fd402ff691ecdb
Nowlook4D <contact@nowlook.fr>
2024-09-12 15:14:48 +0200

    fix H*6

--- scripts/s1.js
+++ scripts/s1.js
@@ -234,7 +234,7 @@ api.addEventListener('viewerready', () => {
     document.getElementById("H").addEventListener("input", function(){ //au curseur, monte ou descend le corps
         if (nomBase1 != "tbdBase") {
             move(idBase1, 0, 0, (document.getElementById("H").value * 6));
-            H = document.getElementById("H").value ;
+            H = document.getElementById("H").value * 6;
         }
     });
 
